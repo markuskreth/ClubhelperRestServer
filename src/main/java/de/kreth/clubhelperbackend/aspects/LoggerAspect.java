9232e2272effa78b81829897ff7f030991166fd2
Markus Kreth <markus.kreth@web.de>
2015-07-19 00:01:49 +0200

    Tries to make LoggerAspect work. failure

--- src/main/java/de/kreth/clubhelperbackend/aspects/LoggerAspect.java
+++ src/main/java/de/kreth/clubhelperbackend/aspects/LoggerAspect.java
@@ -1,34 +1,46 @@
 package de.kreth.clubhelperbackend.aspects;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
-@Aspect
+@Component
+//@Aspect
 public class LoggerAspect extends AbstractLoggerAspect {
 
 	private Logger logger = LoggerFactory.getLogger(LoggerAspect.class);
 
-	@Around("execution (* de.kreth.clubhelperbackend.dao.*.*(..))")
-	public void logDao(ProceedingJoinPoint joinPoint) throws Throwable {
-		joinPoint.proceed();
-		logger.info(generateLogMessage(joinPoint).toString());
-	}
+//	@Around("execution (public * de.kreth.clubhelperbackend.dao.*.*(..))")
+//	public void logDao(ProceedingJoinPoint joinPoint) throws Throwable {
+//		joinPoint.proceed();
+//		logger.info(generateLogMessage(joinPoint).toString());
+//	}
+
+//	@Pointcut("execution (public * de.kreth.clubhelperbackend.controller.PersonController.*(..))")
+//	private void invocation(){}
+
+//	@Before("invocation()")
+//	public void logDao(JoinPoint joinPoint) throws Throwable {
+//		logger.info("logInv: " + generateLogMessage(joinPoint).toString());
+//	}
+
+//	@AfterReturning(pointcut = "invocation()",returning= "result")
+//	public void logDao(JoinPoint joinPoint, Object result) throws Throwable {
+//		logger.warn(generateLogMessage(joinPoint).append(" ==> ").append(result).toString());
+//	}
 	
-	@Around("execution (* de.kreth.clubhelperbackend.controller.*.*(..))")
+	@Around("execution (public * de.kreth.clubhelperbackend.controller.*.*(..))")
 	public void logController(ProceedingJoinPoint joinPoint) throws Throwable {
 		joinPoint.proceed();
-		logger.info(generateLogMessage(joinPoint).toString());
+		logger.info("logCon: " + generateLogMessage(joinPoint).toString());
 	}
 	
-	@AfterThrowing(
-	   pointcut = "execution(* de.kreth.clubhelperbackend.*.*(..))",
-	   throwing= "e")
-	public void logExceptions(JoinPoint joinPoint, Throwable e) {
-		logger.error(generateLogMessage(joinPoint).toString(), e);
-	}
+//	@AfterThrowing(
+//	   pointcut = "execution(* de.kreth.clubhelperbackend.*.*(..))",
+//	   throwing= "e")
+//	public void logExceptions(JoinPoint joinPoint, Throwable e) {
+//		logger.error(generateLogMessage(joinPoint).toString(), e);
+//	}
 }
