26377d8cea07e3a25ab1ccb7b6f69e43ad98837d
Markus Kreth <markus.kreth@web.de>
2016-02-26 19:54:46 +0100

    RelativeDao uses ToStringRelative class for debugging

--- src/main/java/de/kreth/clubhelperbackend/dao/RelativeDao.java
+++ src/main/java/de/kreth/clubhelperbackend/dao/RelativeDao.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
@@ -13,26 +14,22 @@ import de.kreth.clubhelperbackend.pojo.Relative;
 @Repository
 public class RelativeDao extends AbstractDao<Relative> implements Dao<Relative> {
 
-	private static final String columnNames[] 		= {"person1", "person2", "TO_PERSON1_RELATION", "TO_PERSON2_RELATION"};
+	private static final String columnNames[] = { "person1", "person2", "TO_PERSON1_RELATION", "TO_PERSON2_RELATION" };
 
-	private static DaoConfig<Relative> daoConfig = new DaoConfig<Relative>("relative", columnNames, new RelativeRowMapper());
-	
+	private static final DaoConfig<Relative> daoConfig = new DaoConfig<Relative>("relative", columnNames,
+			new RelativeRowMapper());
+
 	public RelativeDao() {
 		super(daoConfig);
 	}
 
 	private static class RelativeRowMapper implements RowMapper<Relative> {
-		
+
 		@Override
 		public Relative mapRow(ResultSet rs, int rowNo) throws SQLException {
-			Relative r = new Relative(
-					rs.getLong("_id"), 
-					rs.getLong("person1"), 
-					rs.getLong("person2"), 
-					rs.getString("TO_PERSON1_RELATION"), 
-					rs.getString("TO_PERSON2_RELATION"), 
-					rs.getTimestamp("changed"), 
-					rs.getTimestamp("created"));
+			Relative r = new ToStringRelative(rs.getLong("_id"), rs.getLong("person1"), rs.getLong("person2"),
+					rs.getString("TO_PERSON1_RELATION"), rs.getString("TO_PERSON2_RELATION"),
+					rs.getTimestamp("changed"), rs.getTimestamp("created"));
 			return r;
 		}
 
@@ -45,5 +42,21 @@ public class RelativeDao extends AbstractDao<Relative> implements Dao<Relative> 
 			values.add(obj.getToPerson2Relation());
 			return values;
 		}
+
+		private class ToStringRelative extends Relative {
+
+			private static final long serialVersionUID = 4772529931953029461L;
+
+			public ToStringRelative(long id, long person1Id, long person2Id, String toPerson1Relation,
+					String toPerson2Relation, Date changedDate, Date createdDate) {
+				super(id, person1Id, person2Id, toPerson1Relation, toPerson2Relation, changedDate, createdDate);
+			}
+
+			@Override
+			public String toString() {
+				return getId() + ": " + getPerson2() + " " + getToPerson1Relation() + " --> " + getPerson1();
+			}
+		}
 	};
+
 }
