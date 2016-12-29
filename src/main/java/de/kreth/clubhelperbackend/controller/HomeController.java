23dbc149232060b0f956cf439841023be566c2e6
Markus Kreth <markus.kreth@web.de>
2016-12-29 13:19:41 +0100

    testing bean ClubhelperAuthenticationProvider

--- src/main/java/de/kreth/clubhelperbackend/controller/HomeController.java
+++ src/main/java/de/kreth/clubhelperbackend/controller/HomeController.java
@@ -16,12 +16,17 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import de.kreth.clubhelperbackend.config.ClubhelperAuthenticationProvider;
 
 /**
  * Handles requests for the application home page.
@@ -32,6 +37,9 @@ public class HomeController {
 
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
+	@Autowired
+	protected ClubhelperAuthenticationProvider clubhelperAuthenticationProvider;
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
@@ -39,6 +47,9 @@ public class HomeController {
 	public String home(HttpServletRequest request, Locale locale, Model model) {
 
 		logger.info("Welcome home! The client locale is {}.", locale);
+		PreAuthenticatedAuthenticationToken authentication = new PreAuthenticatedAuthenticationToken("markus", "0773");
+		Authentication authenticate = clubhelperAuthenticationProvider.authenticate(authentication);
+		logger.debug("Authentificated: " + authenticate);
 
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
