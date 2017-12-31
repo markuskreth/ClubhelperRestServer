204ec822a0c2d707c1a6c5c0610fbe7213e555b9
Markus Kreth <markus.kreth@web.de>
2017-12-31 20:55:52 +0100

    First Version PersonCompetition Table tested.

--- src/main/java/de/kreth/clubhelperbackend/pojo/PersonCompetition.java
+++ src/main/java/de/kreth/clubhelperbackend/pojo/PersonCompetition.java
@@ -66,6 +66,59 @@ public class PersonCompetition extends AbstractData {
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((calenderId == null) ? 0 : calenderId.hashCode());
+		result = prime * result + ((comment == null) ? 0 : comment.hashCode());
+		result = prime * result + ((participation == null) ? 0 : participation.hashCode());
+		result = prime * result + ((personId == null) ? 0 : personId.hashCode());
+		result = prime * result + ((routine == null) ? 0 : routine.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PersonCompetition other = (PersonCompetition) obj;
+		if (calenderId == null) {
+			if (other.calenderId != null)
+				return false;
+		} else if (!calenderId.equals(other.calenderId))
+			return false;
+		if (comment == null) {
+			if (other.comment != null)
+				return false;
+		} else if (!comment.equals(other.comment))
+			return false;
+		if (participation == null) {
+			if (other.participation != null)
+				return false;
+		} else if (!participation.equals(other.participation))
+			return false;
+		if (personId == null) {
+			if (other.personId != null)
+				return false;
+		} else if (!personId.equals(other.personId))
+			return false;
+		if (routine == null) {
+			if (other.routine != null)
+				return false;
+		} else if (!routine.equals(other.routine))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonCompetition [personId=" + personId + ", calenderId=" + calenderId + "]";
+	}
 	
 }
