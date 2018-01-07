2895e87fdd421aefb7b2f038691db9753020899c
Markus Kreth <markus.kreth@web.de>
2018-01-07 04:36:22 +0100

    verbose error message in javascript

--- src/main/webapp/resources/js/ajax_base.js
+++ src/main/webapp/resources/js/ajax_base.js
@@ -7,8 +7,9 @@ var repo = function (requestUrl, targetFunction) {
 		url : encodeURI(requestUrl),
 		dataType : "json",
 	    error: function( jqXHR, textStatus, errorThrown){
-	    	console.log(requestUrl + "\n" + textStatus + "\n" + errorThrown);
-	    	alert(requestUrl + "\n" + textStatus + "\n" + errorThrown);
+	    	var msg = extractError(jqXHR, textStatus, errorThrown);
+	    	console.log(requestUrl + "\n" + msg);
+	    	alert(requestUrl + "\n" + msg);
 	    },
 		success : targetFunction
 	});
@@ -29,10 +30,34 @@ var ajax = function (requestUrl, object, type, resultFunction) {
 	    'processData': withResultFunction,
 	    'success': resultFunction,
 	    'error': function( jqXHR, textStatus, errorThrown){
-	    	console.log(requestUrl + "\n" + textStatus + "\n" + errorThrown);
-	    	alert(requestUrl + "\n" + textStatus + "\n" + errorThrown);
+	    	var msg = extractError(jqXHR, textStatus, errorThrown);
+	    	console.log(requestUrl + "\n" + msg);
+	    	alert(requestUrl + "\n" + msg);
 	    },
 	    'contentType': 'application/json; charset=utf-8' //typically 'application/x-www-form-urlencoded', but the service you are calling may expect 'text/json'... check with the service to see what they expect as content-type in the HTTP header.
 	});
 };
 
+function extractError(jqXHR, textStatus, errorThrown) {
+	var html = $(jqXHR.responseText);
+	var text = [];
+	for (var i=0; i<html.length; i++) {
+		var inner = html[i].innerHTML;
+		if(inner && inner.startsWith("<b>Message</b>")) {
+			inner = inner.replace("<b>Message</b>", "");
+			console.log(i + "->" + inner);
+			text.push(inner);
+			break;
+		} else if(html[i].outerHTML.startsWith("<title>")) {
+			console.log(i + "->" + html[i].innerHTML);
+			text.push(html[i].innerHTML);
+		}
+	}
+
+	if(text.length >0) {
+		return "\n" + text.join("\n");
+	} else {
+		return textStatus + "\n" + errorThrown;
+	}
+	return "";
+}
