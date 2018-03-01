9b86730cdde5c57fc628a73719170a1c59567bee
Markus Kreth <markus.kreth@web.de>
2018-03-01 21:00:44 +0100

    Attendence Result report started

--- src/main/java/de/kreth/clubhelperbackend/pdf/AttendenceBean.java
+++ src/main/java/de/kreth/clubhelperbackend/pdf/AttendenceBean.java
@@ -1,5 +1,6 @@
 package de.kreth.clubhelperbackend.pdf;
 
+import java.text.DateFormat;
 import java.util.Date;
 
 public class AttendenceBean {
@@ -15,5 +16,10 @@ public class AttendenceBean {
 	}
 	public static boolean set() {
 		return true;
+	}
+	
+	@Override
+	public String toString() {		
+		return String.format("%30s;%s;%s%n", name, DateFormat.getDateInstance().format(date), (set?"X":" "));
 	}
 }
