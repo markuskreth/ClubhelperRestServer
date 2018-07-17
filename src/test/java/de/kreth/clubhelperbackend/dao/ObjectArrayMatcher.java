86814a6adafd54a45deded0ca3d250e5a0e86ab4
Markus Kreth <markus.kreth@web.de>
2018-07-17 21:14:16 +0200

    ObjectArrayMatcher with final values.

--- src/test/java/de/kreth/clubhelperbackend/dao/ObjectArrayMatcher.java
+++ src/test/java/de/kreth/clubhelperbackend/dao/ObjectArrayMatcher.java
@@ -7,10 +7,12 @@ import java.util.List;
 import org.mockito.ArgumentMatcher;
 import org.mockito.internal.matchers.VarargMatcher;
 
-public class ObjectArrayMatcher extends ArgumentMatcher<Object[]> implements VarargMatcher {
+public class ObjectArrayMatcher extends ArgumentMatcher<Object[]>
+		implements
+			VarargMatcher {
 
 	private static final long serialVersionUID = -4022372867173708517L;
-	private Object[] values = null;
+	private final Object[] values;
 
 	public ObjectArrayMatcher(Object[] values) {
 		super();
@@ -25,7 +27,7 @@ public class ObjectArrayMatcher extends ArgumentMatcher<Object[]> implements Var
 			} else {
 				return false;
 			}
-		} 
+		}
 		if (argument == null) {
 			return false;
 		}
@@ -36,17 +38,17 @@ public class ObjectArrayMatcher extends ArgumentMatcher<Object[]> implements Var
 		errMsg.append(objArrayToString(values));
 		errMsg.append("; actual=");
 		errMsg.append(objArrayToString(argValues));
-		
-		assertEquals(errMsg.toString(), values.length,
-				argValues.length);
+
+		assertEquals(errMsg.toString(), values.length, argValues.length);
 		for (int i = 0; i < values.length; i++) {
-			assertEquals("At index " + i + " values differ.", values[i], argValues[i]);
+			assertEquals("At index " + i + " values differ.", values[i],
+					argValues[i]);
 		}
 		return true;
 	}
 
 	private String objArrayToString(Object[] v) {
-		if(v == null) {
+		if (v == null) {
 			return "NULL";
 		}
 		StringBuilder bld = new StringBuilder("[");
